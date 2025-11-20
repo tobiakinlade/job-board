c03a8ab0b75937392d1ae62e188fb1c1900fd404
“Tobi <tobi.akinlade.co@gmail.com>
2025-11-20 22:24:19 +0000

    feature: Add Kustomise structure and ArgoCD application for GitOps

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -20,6 +20,7 @@ output "eks_cluster_id" {
   value       = module.eks.cluster_id
 }
 
+
 output "eks_cluster_endpoint" {
   description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
